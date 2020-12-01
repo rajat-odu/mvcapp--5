eb0ed3cb192d103242c07460f7a50f8168a8cf75
Rajat Singh <rajat.odu@gmail.com>
2020-12-01 15:35:59 -0500

    initial commit

--- mvcapp/Models/StudentMemoryDataSet.cs
+++ mvcapp/Models/StudentMemoryDataSet.cs
@@ -12,9 +12,9 @@ namespace mvcapp.Models
         {
             dtset = new List<Student>()
             {
-                new Student {Id=101,Name="User1", CourseId=1},
-                new Student {Id=102,Name="User2", CourseId=2},
-                new Student {Id=103,Name="User3", CourseId=3}
+                new Student {Id=1111111,Name="User11", CourseId=11},
+                new Student {Id=2222222,Name="User22", CourseId=22},
+                new Student {Id=3333333,Name="User33", CourseId=33}
             };
         }
 
